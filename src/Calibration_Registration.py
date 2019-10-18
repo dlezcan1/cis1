2a12a72f07a488ba2382977b2cba79bd27b02c65
Dimitri Lezcano <dlezcan1@jhu.edu>
2019-10-18 17:08:46 -0400

    Began working on the pivot calibration
    Signed-off-by: Dimitri Lezcano <dlezcan1@jhu.edu>

--- src/Calibration_Registration.py
+++ src/Calibration_Registration.py
@@ -7,3 +7,27 @@ This module is to provide python functions of calibration and registration for c
 integrated surgical interventions.
 '''
 
+import transforms3d
+import numpy as np
+
+
+def pointer_calibration(transformation_list: list):
+    """Function that determines the parameters of the pointer given a pivot calibration
+    data for the pointer. 
+    I plan to use the linear least squares solver provided by numpy
+    
+    @param transformation_list: A list of the different transformation matrices from the 
+                                pivot calibration
+    
+    @return: Returns both 
+    
+    """
+    rotations = []
+    translations = []
+    for transform in transformation_list:
+        #split the transformations into their base matrices and vectors
+        R, _, p = transforms3d.affines.decompose44(transform) #leave out the zoom, assumed to be 1's
+        
+        
+        
+        
