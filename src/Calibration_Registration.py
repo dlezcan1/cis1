d139e8df821f69bbd7cc864787b9680c3147952d
Dimitri Lezcano <dlezcan1@jhu.edu>
2019-11-04 18:32:10 -0500

    Removed redundant point-cloud registration function.
    Signed-off-by: Dimitri Lezcano <dlezcan1@jhu.edu>

--- src/Calibration_Registration.py
+++ src/Calibration_Registration.py
@@ -105,63 +105,6 @@ def generate_berntensor( X: np.ndarray, qmin: float, qmax: float, order: int ):
     
 # generate_berntensor
 
-
-def point_cloud_reg_SVD( a, b ):
-    """ This function read the two coordinate systems and
-        calculate the point-to-point registration function.
-        This algorithm is explained in class and implemented as taught.
-        The relationship between a, b, and the result ("F") is
-            b = F a
-        This function will use an SVD in order to determine the corresponding
-        frame transformation from quaternions
-
-        @author: Dimitri Lezcano
-
-        @param a: the input, numpy array where the vectors are the rows of the
-                  matrix
-                  
-        @param b: the corresponding output, numpy array where the vectors are
-                  the rows of the matrix
-
-        @return: F which is a dictionary consist of 'Ratation' as a rotation
-                matrix and 'Trans' as a translational vector
-    
-    """
-    mean_a = np.mean( a, axis = 0 )
-    mean_b = np.mean( b, axis = 0 )
-    
-    # Compute for mean and subtract from a, b, respectively
-    a_hat = a - mean_a
-    b_hat = b - mean_b
-    
-    M = np.empty( ( 0, 4 ) )
-    for ai, bi in zip( a_hat, b_hat ):
-        top_left = 0
-        top_right = bi - ai
-        bottom_left = top_right.reshape( ( -1, 1 ) )
-        bottom_right = transforms3d_extend.skew( top_right )
-        top = np.append( top_left, top_right )
-        bottom = np.hstack( ( bottom_left, bottom_right ) )
-        Mi = np.vstack( ( top, bottom ) )
-        M = np.append( M, Mi, axis = 0 )
-    
-    # for
-    
-    u, s, v = np.linalg.svd( M )
-    
-    q = v[:, 3]
-    
-    R = transforms3d_extend.quaternions.quat2mat( q )
-    
-    p = mean_b - R.dot( mean_a )
-    
-    F = {'Rotation': R, 'Trans': p}
-    
-    return F
-    
-# point_cloud_reg_SVD
-
-
 def point_cloud_reg_Arun( a, b ):
     """ This function read the two coordinate systems and
         calculate the point-to-point registration function.
