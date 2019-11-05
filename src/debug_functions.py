103e2ce107f87b15c1a92d13d1c9272ec0ad2eae
Dimitri Lezcano <dlezcan1@jhu.edu>
2019-11-05 01:08:26 -0500

    removed useless function
    Signed-off-by: Dimitri Lezcano <dlezcan1@jhu.edu>

--- src/debug_functions.py
+++ src/debug_functions.py
@@ -229,18 +229,6 @@ def debug_correct_C():
     
 # debug_correct_C
 
-def debug_compute_emfiducial():
-    print(25*'=', ' compute fiducial points in em coord system ', 25*'=')
-
-    file_name_empivot = "../pa1-2_data/pa2-debug-a-empivot.txt"
-    file_name_calreadings = "../pa1-2_data/pa2-debug-a-calreadings.txt"
-    file_name_emfiducials = "../pa1-2_data/pa2-debug-a-em-fiducialss.txt"
-    file_name_output1 = "../pa1-2_data/pa2-debug-a-output1.txt"
-    print("reading em fiducial data...")
-    _, t_post = Program2.improved_empivot_calib( file_name_empivot)
-    coef, qmin, qmax = Program2.undistort_emfield( file_name_calreadings, file_name_output1, 5)
-    #Program2.compute_fiducials_em(file_name_emfiducials, coef, qmin, qmax, t_post)
-
 def debug_undistort_emfield():
     """Function to debug 'Program2.undistort_emfield' function"""
     filename_calreadings = '../pa1-2_data/pa2-debug-a-calreadings.txt'
